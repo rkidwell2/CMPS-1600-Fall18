1ad243ba8680783bcfa92264aa4fccc0e8c1c24c
Rosalind Kidwell <36898568+rkidwell2@users.noreply.github.com>
2018-11-05 22:37:14 -0600

    Add files via upload

---
+++ CLinkedList.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list_node{
+	int data;
+	struct list_node* next;
+} ListNode;
+
+
+void addToStart(ListNode* head, int newData ){
+    //Passes the reference to the start of the list, and the data the new node will hold
+
+    ListNode* temp = NULL;
+    temp = (ListNode*)malloc(sizeof(ListNode));
+
+    temp->data = head->data;
+    temp->next = head->next;
+
+    head->next = temp;
+    head->data = newData;
+
+}
+
+void removeHead(ListNode* head){
+    //Passes the reference to the start of the list and removes it, making the next item the head
+    ListNode* temp = NULL;
+    temp = (ListNode*)malloc(sizeof(ListNode));
+
+    temp->data = head->data;
+    temp->next = head->next;
+
+    head->data = temp->next->data;
+    head->next = temp->next->next;
+
+    free(temp);
+
+}
+
+int main()
+{
+	ListNode* head = NULL;
+    ListNode* second = NULL;
+
+    head = (ListNode*)malloc(sizeof(ListNode));
+    second = (ListNode*)malloc(sizeof(ListNode));
+
+    head->data = 1;
+    second->data = 2;
+
+    head->next = second;
+
+    printf("List: %d, %d\n", head->data, head->next->data);
+
+	addToStart(head, 8);
+
+    printf("List: %d, %d, %d\n", head->data, head->next->data, head->next->next->data);
+
+    addToStart(head, 15);
+
+    printf("List: %d, %d, %d, %d \n", head->data, head->next->data, head->next->next->data, head->next->next->next->data);
+
+    removeHead(head);
+
+    printf("List: %d, %d, %d\n", head->data, head->next->data, head->next->next->data);
+
+
+    return 0;
+}
+
